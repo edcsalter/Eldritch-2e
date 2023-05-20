c5279cc598a91021de039e6041f8097132f7a566
Ed <edcsalter@gmail.com>
2023-05-20 17:53:52 -0400

    Textfields and boxes for initial 3 values

--- Eldritch 2e/ContentView.swift
+++ Eldritch 2e/ContentView.swift
@@ -8,15 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text1 = ""
+    @State var text2 = ""
+    @State var text3 = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            HStack {
+                Group {
+                    TextField("#", text: $text1)
+                        .padding(.leading, 30)
+                        .font(.system(size:50))
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 2)
+                                .frame(width: 100, height: 100)
+                        )
+                        .padding(.leading, 20)
+                    TextField("#", text: $text2)
+                        .padding(.leading, 40)
+                        .font(.system(size:50))
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 2)
+                                .frame(width: 100, height: 100)
+                        )
+                    TextField("#", text: $text3)
+                        .padding(.leading, 30)
+                        .font(.system(size:50))
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 2)
+                                .frame(width: 100, height: 100)
+                        )
+                        .padding(.trailing, 20)
+                }
+            }
+            .padding(.top, 40)
         }
-        .padding()
+        .padding(.top, 60)
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
