154e37b7ceab13bc945164599d4093050052e90e
Ed <edcsalter@gmail.com>
2023-05-20 14:07:31 -0400

    Initial Commit

---
+++ Eldritch 2e/Eldritch_2eApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eldritch_2eApp.swift
+//  Eldritch 2e
+//
+//  Created by Ed Salter on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Eldritch_2eApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
